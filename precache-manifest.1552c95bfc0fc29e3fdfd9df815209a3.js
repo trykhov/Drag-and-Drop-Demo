212078f7c89f660e753071f91a85b932570581df
Try <khovct@gmail.com>
2020-05-21 16:27:49 -0700

    Updates

---
+++ precache-manifest.1552c95bfc0fc29e3fdfd9df815209a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb537302b46acc6179158ad55a9fefc2",
+    "url": "/Drag-and-Drop-Demo/index.html"
+  },
+  {
+    "revision": "829ec544ecde17755edc",
+    "url": "/Drag-and-Drop-Demo/static/css/main.ed9cf334.chunk.css"
+  },
+  {
+    "revision": "cbbaa313e6a4d1662af4",
+    "url": "/Drag-and-Drop-Demo/static/js/2.39786e27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drag-and-Drop-Demo/static/js/2.39786e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "829ec544ecde17755edc",
+    "url": "/Drag-and-Drop-Demo/static/js/main.a83b9a35.chunk.js"
+  },
+  {
+    "revision": "f7267b9874885724c4dc",
+    "url": "/Drag-and-Drop-Demo/static/js/runtime-main.b1b7597e.js"
+  }
+]);
